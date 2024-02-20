7fa68fd9cc56c41fa1ca448c83e0cca697239165
hramos04 <116158805+hramos04@users.noreply.github.com>
2024-02-20 21:20:55 +0000

    tpc1 completo

--- TPC1/paginaRua.py
+++ TPC1/paginaRua.py
@@ -2,7 +2,7 @@ import os
 import xml.etree.ElementTree as ET
 import re
 
-def escreveParagrafos(elemento):
+def paragrafos(elemento):
     html = ""
     html += f'<p>'     
     for element in elemento.iter():
@@ -18,12 +18,10 @@ def escreveParagrafos(elemento):
     return html
 
 
-def gerar_pagina_html(arquivo_xml):
-    # Parseia o XML
+def paginaIndice(arquivo_xml):
     tree = ET.parse(arquivo_xml)
     root = tree.getroot()
 
-    # Criação do HTML
     html = '<!DOCTYPE html>\n'
     html += '<html lang="en">\n'
     html += '<head>\n'
@@ -33,27 +31,28 @@ def gerar_pagina_html(arquivo_xml):
     html += '</head>\n'
     html += '<body>\n'
 
-    # Adiciona a meta informação
     meta = root.find('meta')
     numero = meta.find('número').text
     nome = meta.find('nome').text
     html += f'<h1>Rua {numero}: {nome}</h1>\n'
 
-    # Adiciona o corpo do XML ao HTML
     html += '<div>\n'
     corpo = root.find('corpo')
+    
+
+    html += '<b>Antigamente:</b>\n'  
     for elemento in corpo:
-        if elemento.tag == 'figura':
+        if elemento.tag == 'figura' :
             imagem = elemento.find('imagem').attrib['path']
             legenda = elemento.find('legenda').text
-            imagem_path= imagem[3:]
+            imagem_path = imagem[3:]
             html += f'<figure>\n'
-            html += f'<img src=../MapaRuas-materialBase/{imagem_path} alt="{legenda}">\n'
+            html += f'<img src=../MapaRuas-materialBase/{imagem_path} alt="{legenda}" height=150px>\n'
             html += f'<figcaption>{legenda}</figcaption>\n'
             html += f'</figure>\n'
             os.chdir(os.path.dirname(__file__))
         elif elemento.tag == 'para':
-            html+= escreveParagrafos(elemento)
+            html += paragrafos(elemento)
         elif elemento.tag == 'lista-casas':
             html += '<ul>\n'
             for casa in elemento.findall('casa'):
@@ -70,9 +69,9 @@ def gerar_pagina_html(arquivo_xml):
 
                 if casa.find('desc') is not None:
                     for elem in casa.find('desc'):
-                        desc= escreveParagrafos(elem)
+                        desc = paragrafos(elem)
                 else:
-                    desc="N/A"
+                    desc = "N/A"
                     
                 html += f'<li>Número da casa: {numero_casa}, Enfiteuta: {enfiteuta}, Foro: {foro}, Descricao: {desc}</li>\n'
             html += '</ul>\n'
@@ -83,19 +82,6 @@ def gerar_pagina_html(arquivo_xml):
     html += '</html>\n'
     return html
 
-def extrair_numero_do_arquivo(nome_arquivo):
-    # Expressão regular para encontrar o número no padrão desejado
-    padrao = r'MRB-(\d+)-.*\.xml'
-    
-    # Tenta encontrar uma correspondência com o padrão
-    correspondencia = re.match(padrao, nome_arquivo)
-    
-    if correspondencia:
-        # Retorna o número encontrado
-        numero = correspondencia.group(1)
-        return numero
-    else:
-        return None
 
 pasta_xml = "C:/Users/hugoa/Desktop/Escola/Universidade/3ºano/2 semestre/Engenharia Web/EngWeb2024/TPC1/MapaRuas-materialBase/texto"
 pasta_html = "C:/Users/hugoa/Desktop/Escola/Universidade/3ºano/2 semestre/Engenharia Web/EngWeb2024/TPC1/Ruas"
@@ -109,11 +95,9 @@ else:
 for arquivo_xml in os.listdir(pasta_xml):
     if arquivo_xml.endswith(".xml"):
         caminho_arquivo_xml = os.path.join(pasta_xml, arquivo_xml)
-        nome_arquivo_html = f"rua_{arquivo_xml[:-4]}.html"  # Removendo a extensão .xml e adicionando prefixo "rua_"
+        nome_arquivo_html = f"rua_{arquivo_xml[:-4]}.html"
         caminho_arquivo_html = os.path.join(pasta_html, nome_arquivo_html)
-        html_resultante = gerar_pagina_html(caminho_arquivo_xml)
+        html_resultante = paginaIndice(caminho_arquivo_xml)
         with open(caminho_arquivo_html, "wb") as f:
             f.write(html_resultante.encode('utf-8'))
         print(f"Arquivo HTML '{nome_arquivo_html}' criado com sucesso.")
-
-print("Conversão concluída. Os arquivos HTML foram salvos na pasta 'html'.")
